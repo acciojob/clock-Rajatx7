e0119cee6df3287ae5b51943feb5fdc823561db4
Rajat Singh <155432801+Rajatx7@users.noreply.github.com>
2024-02-05 21:51:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+  function updateClock() {
+      const currentDate = new Date();
+      const dateTimeElement = document.getElementById("liveClock");
+      dateTimeElement.textContent = `${currentDate.toLocaleString()}`;
+    }
+
+     // Update the clock every second (1000 milliseconds)
+  setInterval(updateClock, 1000);
+
+  // Initial call to display the clock immediately
+  updateClock();
